871f592ab7a9aca2ed7f9823887a1f9e6975feae
xyz2022 <91395091+xyz2022@users.noreply.github.com>
2023-05-25 09:53:21 +1000

    Initial commit

---
+++ Assets/_My/Scripts/AnimationController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class AnimationController : StateMachineBehaviour
+{
+
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("pressedLP", false);
+        animator.SetBool("pressedRP", false);
+        animator.SetBool("pressedLK", false);
+        animator.SetBool("pressedRK", false);
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
---
+++ Assets/_My/Scripts/InputDirector.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputDirector : MonoBehaviour
+{
+
+
+    public KeyCode keyUp;
+    public KeyCode keyDown;
+    public KeyCode keyLeft;
+    public KeyCode keyRight;
+    public KeyCode keyLPunch;
+    public KeyCode keyRPunch;
+    public KeyCode keyLKick;
+    public KeyCode keyRKick;
+
+    private InputDirector inputDirector;
+    private Animator animator;
+
+    void Awake()
+    {
+        inputDirector = GetComponentInChildren<InputDirector>();
+        animator = GetComponentInChildren<Animator>();
+
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(keyLPunch))
+        {
+            animator.SetBool("pressedLP", true);
+        }
+        if (Input.GetKeyDown(keyRPunch))
+        {
+            animator.SetBool("pressedRP", true);
+        }
+        if (Input.GetKeyDown(keyLKick))
+        {
+            animator.SetBool("pressedLK", true);
+        }
+        if (Input.GetKeyDown(keyRKick))
+        {
+            animator.SetBool("pressedRK", true);
+        }
+    }
+
+
+}
